cf6536f14197647c163528f6dedd7df54a3dc00f
Ananth Venkatesh <46249765+quantum9Innovation@users.noreply.github.com>
2024-01-18 21:42:01 +0000

    Initial package commit

---
+++ splitcycle/__init__.py
@@ -0,0 +1,7 @@
+'''
+"Split Cycle: A New Condorcet Consistent Voting Method Independent of Clones and Immune to Spoilers"
+Wes Holliday and Eric Pacuit
+
+Adaptation as a Python package
+Open source contributions by Ananth Venkatesh and Youwen Wu
+'''
